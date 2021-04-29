b4258a934c43941736f3ce5ecf085b09192b3426
2021-01-06 <77026510+2021-01-06@users.noreply.github.com>
2021-04-29 12:58:56 +0900

    Add files via upload

--- 2960.py
+++ 2960.py
@@ -1,18 +1,18 @@
-# # 에라토스테네스의 채
-# N, K = map(int, input().split())
-# # k번째 지워진 수를 출력
-# s = [False, False] + [True] * (N-1)
-# c = 0
-# a = 0
-# for i in range(2, N+1):
-#     if s[i]:
-#         c += 1
-#         if c == K:
-#             a = j
-#         for j in range(2*i, N+1, i):
-#             if s[j]:
-#                 c += 1
-#                 s[j] = False
-#                 if c == K:
-#                   a = j
-# print(a)
+# 에라토스테네스의 채
+N, K = map(int, input().split())
+
+check = [False, False] + [True] * (N-1)
+
+eNum = 0
+result = 0
+for i in range(2, N+1):
+    if check[i] == True:
+        for j in range(i, N+1, i):
+            if check[j] == True:
+                check[j] = False
+                eNum += 1
+                if eNum == K:
+                    result = j
+    else:
+        pass
+print(result)
